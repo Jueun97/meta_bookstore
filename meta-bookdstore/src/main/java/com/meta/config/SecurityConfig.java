b6946e113e5fad79edaf1652fd36e284b6d69b2b
keyminy <imanaifs@gmail.com>
2022-01-24 21:22:46 +0900

    feat:관리자 로그인시 이동url 설정 완료

--- meta-bookdstore/src/main/java/com/meta/config/SecurityConfig.java
+++ meta-bookdstore/src/main/java/com/meta/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.loginPage("/member/login")
 			.loginProcessingUrl("/member/login")
 			.failureUrl("/member/login?error")
-			.defaultSuccessUrl("/main/main")
+			.defaultSuccessUrl("/member/redirect",true)
 			.usernameParameter("id")
 			.passwordParameter("password");
 		
