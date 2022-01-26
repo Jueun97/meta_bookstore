49cc0dfcb88fab00f80746ee9833c6a9c0286702
Jueun <zxnm0901@naver.com>
2022-01-27 08:58:28 +0900

    Update BookVO.java

--- meta-bookdstore/src/main/java/com/meta/book/vo/BookVO.java
+++ meta-bookdstore/src/main/java/com/meta/book/vo/BookVO.java
@@ -1,17 +1,17 @@
 package com.meta.book.vo;
 
-import java.util.Date;
 
 import lombok.Data;
 
 @Data
 public class BookVO {
-	private Long book_no;
+	private int book_no;
 	private String title;
+	private String image;
 	private String author;
-	private String pub;
-	private Date pubDate;
+	private String price;
+	private String publisher;
+	private String pubdate;
 	private String description;
-	private int price;
 	private int cate_no;
 }
