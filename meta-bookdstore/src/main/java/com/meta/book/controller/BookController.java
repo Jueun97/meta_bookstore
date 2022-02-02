32df7310a60830343690212d5760f7a92bdd071c
Jueun <zxnm0901@naver.com>
2022-02-02 12:46:22 +0900

    book 테이블 컬럼명 수정

--- meta-bookdstore/src/main/java/com/meta/book/controller/BookController.java
+++ meta-bookdstore/src/main/java/com/meta/book/controller/BookController.java
@@ -3,16 +3,13 @@ package com.meta.book.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.meta.book.service.BookService;
-import com.meta.config.auth.PrincipalDetails;
 
-import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/book")
@@ -29,5 +26,4 @@ public class BookController {
 		model.addAttribute("bookList",service.list());
 		return "book/list";
 	}
-
 }
