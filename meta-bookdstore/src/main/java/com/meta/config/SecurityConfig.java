096e14a11c100782a9870efd3d7b3476b5904a01
DESKTOP-A037TC6\user <yh4268@koreatech.ac.kr>
2022-02-22 17:24:47 +0900

    feat: 마무리

--- meta-bookdstore/src/main/java/com/meta/config/SecurityConfig.java
+++ meta-bookdstore/src/main/java/com/meta/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		auth.userDetailsService(userDetailsService)
 			.passwordEncoder(encode());
 	}
-	 
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
